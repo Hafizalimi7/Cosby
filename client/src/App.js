46b03ab1e5ccbfeb94f3009c33b9ef8206076607
Hafiz <hafizalimi77@gmail.com>
2022-11-27 14:35:44 +0000

    added not-found page *added styling to page *page redirect to not-found when entered wrong url

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/cart" element={<Cart/>} />
-          <Route path="/" element={<Home/>} />
+          <Route path="/not-found" element={<NotFound/>} />
+          <Route path="/" exact element={<Home/>} />
+          <Route to="/not-found" />
         </Routes>
       </BrowserRouter>
     </div>
