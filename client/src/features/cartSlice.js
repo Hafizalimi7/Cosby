f979282932e35c3884f76b8edec644baccf2542e
Hafiz <hafizalimi77@gmail.com>
2022-12-17 17:34:51 +0000

    can now remove item from cart

--- client/src/features/cartSlice.js
+++ client/src/features/cartSlice.js
@@ -20,22 +20,34 @@ const cartSlice = createSlice({
           ...state.cartItems[existingIndex],
           cartQuantity: state.cartItems[existingIndex].cartQuantity + 1,
         };
-        toast.info("Increased product quantity", {
+        toast.info(`${action.payload.name} increased cart quantity`, {
           position: "bottom-left",
         });
       } else {
         let tempProductItem = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(tempProductItem);
-        toast.success("Product added to cart", {
+        toast.success(`${action.payload.name} added to cart`, {
           position: "bottom-left",
         });
         }
 
         localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
     },
+    removeFromCart(state, action){
+      const nextCartItems = state.cartItems.filter(
+        cartItem => cartItem.id !== action.payload.id
+      )
+
+      state.cartItems = nextCartItems
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
+
+      toast.error(`${action.payload.name} removed from cart`, {
+        position: "bottom-left",
+      });
+    },
   },
 });
 
-export const {addToCart } = cartSlice.actions;
+export const {addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
