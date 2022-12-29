609bd0f5c436be89558927012ce68927325deef4
Hafiz <hafizalimi77@gmail.com>
2022-12-29 11:08:42 +0000

    connects to mongoDb *created a schema *required dotenv

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const cors = require("cors")//cors relaxs the security applied to an API
+const mongoose = require("mongoose")
 
 
 const product = require("./products")//from product file
 
 const app = express()//creating an object to call different methods
 
-
+require("dotenv").config()
 
 app.use(express.json())//configuring a middleware , inccrease the functionailty
 app.use(cors())//allows to access to nodejs api from react application
@@ -19,7 +20,15 @@ app.get("/products", (req,res) => {
   res.send(product)//sends data from product file and shows on browser
 })
 const port = process.env.PORT || 5000
-
+const uri = process.env.DB_URI
 
 app.listen(port, console.log(`server is running on port ${port}`))//shows when backend port is running
 
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDb Connection Successful"))
+  .catch((err) => console.log("Failed To Connect", err.message));//shows when connection failed
