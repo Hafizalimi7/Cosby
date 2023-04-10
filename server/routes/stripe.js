fe48239c6d022a9ec982b14abce9d42870c40f54
Hafiz <hafizalimi77@gmail.com>
2023-04-10 14:35:08 +0100

    now shows items from the frontend checkout

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -6,19 +6,26 @@ const stripe =  Stripe(process.env.STRIPE_KEY)
 const router = express.Router();
 
 router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000
+
+  const line_items = req.body.cartItems.map(item => {
+    return{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata:{
+            id: item.id
+          }
         },
-        quantity: 1,
+        unit_amount: item.price * 100
       },
-    ],
+      quantity: item.cartQuantity,
+    }
+  })
+  const session = await stripe.checkout.sessions.create({
+    line_items,
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
     cancel_url: "http://localhost:3000/cart",
